16439be045f35a52020b5a27d30c957998358d69
Bob-Yeah <635596704@qq.com>
2017-08-01 20:22:55 +0800

    add some code

--- CameraCali/main.cpp
+++ CameraCali/main.cpp
@@ -7,5 +7,16 @@
 using namespace std;
 using namespace cv;
 int main(int argc, char* argv) {
+	VideoCapture cap(1);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+	if (!cap.isOpened()) {
+		cout << "打开摄像头失败，退出";
+		exit(-1);
+
+	}
+	namedWindow("CameraCali");
+	cout << "Press g to start capture images!"<<endl;
+
 
 }
